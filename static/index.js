7aae6e3dbc8f2a1dfb4fa6c316afe258fff700b6
qwer5418812321 <98463865+qwer5418812321@users.noreply.github.com>
2022-01-27 09:51:33 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -414,16 +414,16 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             if (score <= 5) return '标哥狠生气';
             if (score <= 8) return '文鹏很生气
 ';
-            if (score <= 10)  return 'TQL';
+            if (score <= 10)  return '。。。。';
             if (score <= 15) return '您是江文鹏？
 ';
-            return '人？';
+            return '你是本人？';
         } else {
-            if (score <= 49) return '试着好好练一下？';
-            if (score <= 99) return 'TCL';
-            if (score <= 149) return 'TQL';
-            if (score <= 199) return '您';
-            return '人？';
+            if (score <= 49) return '标哥狠生气';
+            if (score <= 99) return '文鹏很生气';
+            if (score <= 149) return '。。。。';
+            if (score <= 199) return '您是江文鹏？';
+            return '你是本人？';
         }
     }
 
